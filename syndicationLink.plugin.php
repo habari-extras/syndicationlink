cf7984fd4a55eef294198c659caf8dfe9a087c76
Mark Mynsted <code@growingliberty.com>
2012-03-08 23:45:43 -0600

    Updated to permit truncation
    Updated to permit truncation of articles with a direct link back to the
    article.  Defaults to no truncation by way of a default of zero for the
    truncation length, where zero truncation length means show the entire
    article.

--- syndicationLink.plugin.php
+++ syndicationLink.plugin.php
@@ -10,8 +10,42 @@ extends Plugin
 {	
   public function filter_atom_add_post($content, $post )
   {
-    return $content . "<br/><br/><a href=\"" . Site::get_url( 'habari' ) . 
-      "\">" . Utils::htmlspecialchars( Options::get( 'title' ) ) . "</a><br/><br/><br/>";
+    $cCount = Options::get('syndicationLink__truncateAfter');
+
+    if ($cCount == 0)
+      return $content . "<br/><br/><a href=\"" . Site::get_url( 'habari' ) . "\">" . 
+	Utils::htmlspecialchars( Options::get( 'title' ) ) . "</a><br/><br/><br/>";
+    else
+      return self::truncate_article($content, (int) $cCount, $post->permalink);
+  }
+
+  /* 
+   * truncate an article after about some number of characters
+   * and provide link back directly to truncated article.
+   */
+  function truncate_article($textIn, $characterCount, $link) 
+  {
+    if(strlen($textIn) > $CharacterCount) 
+      {
+   	$textIn = substr($textIn, 0, $characterCount);
+   	$textIn .= $textIn . " . . . <br/><br/><a href=\"" . $link . "\">" . 
+   	"Complete article</a><br/><br/><br/>";
+      }
+    return $textIn;
+
+  }
+
+  /*
+   * Request number of characters after which to truncate articles in syndication
+   * Zero, or undefined means 'do not truncate'.
+   */
+  public function configure()
+  {
+    $ui = new FormUI( 'syndicationLink_form' );
+    $ui->append( 'text', 'truncateAfter', 'option:syndicationLink__truncateAfter',
+		 _t('Truncate after a number of characters, zero means display all:', 'plugin_locale') );
+    $ui->append('submit', 'save', _t('Save', 'plugin_locale'));
+    return $ui;
   }
 }
 ?>
